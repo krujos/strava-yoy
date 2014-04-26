5eafac6ff8a5782362d09b0ba7c1b719e042d54c
jkruck <joshua_kruck@symantec.com>
2014-04-25 21:50:29 -0500

    add login

--- stravayoy.py
+++ stravayoy.py
@@ -1,5 +1,5 @@
 __author__ = 'jkruck'
-from flask import Flask, redirect, url_for, request, session
+from flask import Flask, redirect, url_for, request, session,abort
 from strava import strava_utils
 
 Flask.get = lambda self, path: self.route(path, methods=['get'])
@@ -10,11 +10,14 @@ Flask.delete = lambda self, path: self.route(path, methods=['delete'])
 app = Flask(__name__)
 client_secret, client_id = strava_utils.get_settings()
 app.secret_key = client_secret
+redirect_url = "http://127.0.0.1:5000"
 
 
 def do_token_exchange(code):
     session.permanent = True
     response = strava_utils.get_token(code)
+    if 'message' in response and response['message'] == 'Authorization Error':
+        abort(401)
     session['token'] = response['access_token']
     athlete = response['athlete']
     session['athlete_id'] = athlete['id']
@@ -29,6 +32,13 @@ def get_root():
         return redirect(url_for('static', filename='loggedin.html'))
 
     return redirect(url_for('static', filename='index.html'))
+
+
+@app.get('/login')
+def login():
+    return redirect("%s?client_id=%s&response_type=code&redirect_uri=%s&scope=view_private"
+                    % (strava_utils.OAUTH_URL, client_id, redirect_url))
+
 
 if __name__ == "__main__":
     app.debug = True
