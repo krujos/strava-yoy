90469ab65299726e60ce261ff9c4c0a5fedbe791
krujos <joshua.kruck@gmail.com>
2014-06-17 23:12:39 -0500

    Stub in date picker

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,52 @@
 'use strict';
 
-var stravaApp = angular.module('stravaApp', []);
+var stravaApp = angular.module('stravaApp', ['ui.bootstrap']);
 
 // Main app controller.
 stravaApp.controller('StravaController', function StravaController($scope, $http) {
-
     $scope.login = function () {
         window.location.replace('/login');
     };
 });
 
+stravaApp.controller('UserController', function UserController($scope, $http) {
+
+    $scope.username = "loading....";
+
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.initDate = new Date('2016-15-20');
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+
+});
 
