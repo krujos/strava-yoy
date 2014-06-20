63fa5c1d6a1265b6e731ce520f0a1600f3a7679e
krujos <joshua.kruck@gmail.com>
2014-06-20 00:18:32 -0500

    Beginnings of docker file, still issues w/ boot2docker

--- static/js/test/test.sh
+++ static/js/test/test.sh
@@ -4,4 +4,4 @@ echo ""
 echo "Starting Karma Server (http://karma-runner.github.io)"
 echo "-------------------------------------------------------------------"
 
-../node_modules/.bin/karma start ../test/config/karma.conf.js $*
+node_modules/.bin/karma start config/karma.conf.js $*
